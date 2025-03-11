6a116dad1360e4841b3e38a7cca17605e3faf6a5
syeonih <syeonih@naver.com>
2025-03-11 14:17:19 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">
+        Hello, Tailwind CSS 4.0!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
